43cc47bc53afe83e395c54c8b79fa203a76c50b5
balasaravanan3502 <balasaravananvp35@gmail.com>
2022-11-09 22:33:53 +0530

    add: model in web page

--- Project Development Phase/Sprint 4/app.py
+++ Project Development Phase/Sprint 4/app.py
@@ -5,6 +5,7 @@ import pickle
 
 
 app= Flask(__name__)
+model = pickle.load(open('/Users/balasaravananvp/Documents/tensorflow-test/IBM-Project-26380-1660025744/Project Development Phase/Sprint 4/model.pkl', 'rb')) # loading the trained model
 
 @app.route("/") 
 def about():
@@ -18,18 +19,21 @@ def home():
 def information():
     return render_template("info.html")
 
-@app.route("/upload")
-def test():
-    return render_template("predict.html")
 
 @app.route("/predict",methods=["GET","POST"]) 
 def upload():
+    print(request)
     if request.method=='POST':
-        print(request)
-        model = pickle.load(open('model.pkl','rb'))
-        # pred=model.predict(x)
-        return 1    
-    return None
+        init_features = [float(x) for x in request.form.values()]
+        final_features = [np.array(init_features)]
+        print(final_features)
+        pred = model.predict(final_features)
+        print(pred)
+        return render_template("result.html",prediction=pred)
+
+    else:
+        return render_template("predict.html")
+
 
 if __name__=="__main__":
     app.run(debug=False,port=5500)
